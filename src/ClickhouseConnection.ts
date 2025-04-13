da73067875105d58e25315ebc2592264619f207f
cg10036 <cg10036@gmail.com>
2025-04-13 14:58:47 +0900

    Fix: handle general insert

--- src/ClickhouseConnection.ts
+++ src/ClickhouseConnection.ts
@@ -44,22 +44,19 @@ export class ClickhouseConnection implements DatabaseConnection {
   }
 
   async executeQuery<O>(compiledQuery: CompiledQuery): Promise<QueryResult<O>> {
-    const queryKind = compiledQuery.query.kind
+    if (compiledQuery.query.kind === 'InsertQueryNode') {
+      // @ts-expect-error: fix types
+      if (!compiledQuery.query.values?.values) {
+        // handle complex query (ex: INSERT INTO ... SELECT ...)
+        const query = this.prepareQuery(compiledQuery)
+        const result = await this.#client.query({
+          query,
+          clickhouse_settings: {
+            date_time_input_format: 'best_effort',
+          },
+        });
 
-    if (queryKind === 'InsertQueryNode' || queryKind === 'UpdateQueryNode' || queryKind === 'SelectQueryNode') {
-      const query = this.prepareQuery(compiledQuery)
-
-      const resultSet = await this.#client.query({
-        query,
-        clickhouse_settings: {
-          ...(queryKind === 'InsertQueryNode' && {
-            date_time_input_format: "best_effort",
-          }),
-        },
-      })
-
-      if (queryKind === 'InsertQueryNode') {
-        const summary = resultSet.response_headers["x-clickhouse-summary"]
+        const summary = result.response_headers['x-clickhouse-summary']
         const summaryObject = JSON.parse(
           (Array.isArray(summary) ? summary[0] : summary) ?? '{}'
         )
@@ -71,6 +68,37 @@ export class ClickhouseConnection implements DatabaseConnection {
         }
       }
 
+      const values = [
+        compiledQuery.query.columns?.map(c => c.column.name) ?? [],
+        // @ts-expect-error: fix types
+        ...compiledQuery.query.values?.values.map(v => v.values) ?? [],
+      ]
+      
+      const schema = compiledQuery.query.into?.table?.schema?.name;
+      const table = compiledQuery.query.into?.table.identifier.name ?? "";
+      const fullQualifiedTable = schema ? `${schema}.${table}` : table
+      const resultSet = await this.#client.insert({
+        table: fullQualifiedTable,
+        format: 'JSONCompactEachRowWithNames',
+        values,
+        clickhouse_settings: {
+          date_time_input_format: 'best_effort',
+        },
+      })
+
+      return {
+        rows: [],
+        numAffectedRows: BigInt(resultSet.summary?.written_rows ?? 0),
+        numChangedRows: BigInt(resultSet.summary?.written_rows ?? 0),
+      }
+    }
+
+    if (compiledQuery.query.kind === 'UpdateQueryNode' || compiledQuery.query.kind === 'SelectQueryNode') {
+      const query = this.prepareQuery(compiledQuery)
+
+      const resultSet = await this.#client.query({
+        query,
+      })
       const response = await resultSet.json()
 
       return {
