0c743b4ef2f27d88c6418fbbc50d1914d44873fc
Makara Sok <me@maktouch.com>
2025-04-15 11:07:21 -0400

    1.6.0: complicated inserts + remove bluebird deps

--- tests/clickhouse.spec.ts
+++ tests/clickhouse.spec.ts
@@ -15,8 +15,32 @@ const kysely = new Kysely<any>({
 })
 
 
+// @todo: make test environment work
 test('introspection', async () => {
   const tables = await kysely.introspection.getTables()
 
   expect(tables).toMatchSnapshot()
+})
+
+test('insert', async () => {
+  const result = await kysely.insertInto('company_metrics').values([{
+    company_id: 1,
+    date: '2025-04-15',
+    mrr: 100,
+  },
+    {
+    company_id: 2,
+    date: '2025-04-15',
+    mrr: 200,
+  }]).execute();
+
+  expect(result).toMatchSnapshot()
+})
+
+test('update', async () => {
+  const result = await kysely.updateTable('company_metrics').set({
+    mrr: 300
+  }).where('company_id', '=', 1).where('date', '=', '2025-04-15').execute();
+
+  expect(result).toMatchSnapshot()
 })
